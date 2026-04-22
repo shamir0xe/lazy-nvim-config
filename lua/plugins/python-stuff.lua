7e031692a9a6ec3ff14b5625cd63fadd21d356b6
shamir0xe <shamir0xe@gmail.com>
2026-04-22 13:12:09 +0330

    feat: Add formatter for Makefile

--- lua/plugins/python-stuff.lua
+++ lua/plugins/python-stuff.lua
@@ -3,9 +3,22 @@ return {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
+      formatters = {
+        mbake = {
+          command = "mbake",
+          args = {
+            "format",
+            "--config",
+            vim.fn.expand("~/.bake.toml"),
+            "$FILENAME",
+          },
+          stdin = false,
+        },
+      },
       formatters_by_ft = {
         ["python"] = { "black" },
         ["yaml"] = { "prettier" },
+        ["make"] = { "mbake" },
       },
     },
   },
